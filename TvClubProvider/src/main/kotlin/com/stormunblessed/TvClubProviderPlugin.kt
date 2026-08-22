f9ff79ecfdbf9dd96e38aa83b28f001db74e7449
soyelmismo <thinkboutme@protonmail.com>
2026-08-22 16:31:37 +0000

    feat(PLPro & TvClub): bump v13, integrate native multi-mirror extractors for StreamWish and VidHide

--- TvClubProvider/src/main/kotlin/com/stormunblessed/TvClubProviderPlugin.kt
+++ TvClubProvider/src/main/kotlin/com/stormunblessed/TvClubProviderPlugin.kt
@@ -3,10 +3,60 @@ package com.stormunblessed
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.stormunblessed.extractors.*
 
 @CloudstreamPlugin
 class TvClubProviderPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(TvClubProvider())
+
+        // VidHide extractors
+        registerExtractorAPI(DhtpreCom())
+        registerExtractorAPI(DingtezuniCom())
+        registerExtractorAPI(Minochinos())
+        registerExtractorAPI(Ryderjet())
+        registerExtractorAPI(VidHideHub())
+        registerExtractorAPI(VidHidePro1())
+        registerExtractorAPI(VidHidePro2())
+        registerExtractorAPI(VidHidePro3())
+        registerExtractorAPI(VidHidePro4())
+        registerExtractorAPI(VidHidePro5())
+        registerExtractorAPI(VidHidePro6())
+        registerExtractorAPI(Smoothpre())
+        registerExtractorAPI(Dhtpre())
+        registerExtractorAPI(Peytonepre())
+        registerExtractorAPI(VidHidePro())
+
+        // StreamWish extractors
+        registerExtractorAPI(SaveFiles())
+        registerExtractorAPI(Mwish())
+        registerExtractorAPI(Dwish())
+        registerExtractorAPI(Ewish())
+        registerExtractorAPI(WishembedPro())
+        registerExtractorAPI(Kswplayer())
+        registerExtractorAPI(Wishfast())
+        registerExtractorAPI(Streamwish2())
+        registerExtractorAPI(SfastwishCom())
+        registerExtractorAPI(Strwish())
+        registerExtractorAPI(Strwish2())
+        registerExtractorAPI(FlaswishCom())
+        registerExtractorAPI(Awish())
+        registerExtractorAPI(Obeywish())
+        registerExtractorAPI(Jodwish())
+        registerExtractorAPI(Swhoi())
+        registerExtractorAPI(Multimovies())
+        registerExtractorAPI(UqloadsXyz())
+        registerExtractorAPI(Doodporn())
+        registerExtractorAPI(CdnwishCom())
+        registerExtractorAPI(Asnwish())
+        registerExtractorAPI(Nekowish())
+        registerExtractorAPI(Nekostream())
+        registerExtractorAPI(Swdyu())
+        registerExtractorAPI(Wishonly())
+        registerExtractorAPI(Playerwish())
+        registerExtractorAPI(StreamHLS())
+        registerExtractorAPI(HlsWish())
+        registerExtractorAPI(StreamWishExtractor())
     }
 }
+
